2aa6195cdf8ddaef7da801cd9a4dfa7cf87e7323
samwel <samwuelkinuthia@gmail.com>
2017-09-27 19:00:43 +0300

    stored color list and looped to assign color values to each square

---
+++ script.js
@@ -0,0 +1,17 @@
+//array of colours to be selected.
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(255, 255, 10)",
+  "rgb(255, 20, 180)",
+  "rgb(255, 50, 250)"
+]
+
+//selecting all six colours defined in var colors.
+var squares = document.querySelectorAll(".square");
+
+//loop for every color, set background as value of i.
+for(var i = 0; i < squares.length; i++){
+  squares[i].style.background = colors[i]
+}
