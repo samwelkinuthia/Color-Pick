6e2e38fe35f0bf5fa06d2826c4b53a648f64b615
samwel <samwuelkinuthia@gmail.com>
2017-10-07 19:32:57 +0300

    set textcontent to play again after hardBtn click

--- colorpick.js
+++ colorpick.js
@@ -17,7 +17,7 @@ var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
 //easy button behaviour
-easyBtn.addEventListener("click", function(){
+easyBtn.addEventListener("click", function() {
   hardBtn.classList.remove("selected");
   easyBtn.classList.add("selected");
   //generate 3 random colors
@@ -27,25 +27,39 @@ easyBtn.addEventListener("click", function(){
   //set title background equal to picked color
   colorDisplay.textContent = pickedColor;
   //set new colors for 3 squares and hide the other three
-  for(var i=0; i < squares.length; i++){
-    if(colors[i]){
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
       squares[i].style.background = colors[i]
-    }else{
+    } else {
       squares[i].style.display = "none";
     }
   }
   h1.style.background = "#f2f2f2";
+  resetButton.textContent = "New colors";
 });
 
 
-hardBtn.addEventListener("click", function(){
+hardBtn.addEventListener("click", function() {
   hardBtn.classList.add("selected");
   easyBtn.classList.remove("selected");
+  //generate 3 random colors
+  colors = generateRandomColors(6);
+  //pick color
+  pickedColor = pickColor();
+  //set title background equal to picked color
+  colorDisplay.textContent = pickedColor;
+  //set new colors for 3 squares and hide the other three
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i]
+    squares[i].style.display = "block";
+  }
+  h1.style.background = "#f2f2f2";
+  resetButton.textContent = "New colors";
 });
 
 
 
-resetButton.addEventListener("click", function(){
+resetButton.addEventListener("click", function() {
   //generate all new colors
   colors = generateRandomColors(6);
   //pick a random color
@@ -53,7 +67,7 @@ resetButton.addEventListener("click", function(){
   //change color display to match picked color
   colorDisplay.textContent = pickedColor;
   //change square colors
-  for(var i = 0; i < squares.length; i++){
+  for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
   }
   //reset header background-color
@@ -85,25 +99,25 @@ for (var i = 0; i < squares.length; i++) {
 
 //change colors after successful color select
 
-function changeColors(color){
+function changeColors(color) {
   //loop through all squares, set color to equal selected square
-  for(var i = 0; i < squares.length; i++){
+  for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = color;
   }
 }
 
 //randomize color selection
-function pickColor(){
+function pickColor() {
   var random = Math.floor(Math.random() * colors.length)
   return colors[random];
 }
 //random color generator.
-function generateRandomColors(num){
+function generateRandomColors(num) {
   //empty array to hold the colors.
   var arr = []
   //add colors to the array.
   //repeat num times.
-  for(var i = 0; i < num; i++){
+  for (var i = 0; i < num; i++) {
     //push colors to array
     arr.push(randomColor());
   }
@@ -112,7 +126,7 @@ function generateRandomColors(num){
 }
 
 //the random color generator
-function randomColor(){
+function randomColor() {
   //pick red
   var r = Math.floor(Math.random() * 256);
   //pick green
