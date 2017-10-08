1d289bb98db65e8a07d3c1297a2eb5b5ad863d7e
samwel <samwuelkinuthia@gmail.com>
2017-10-08 13:17:30 +0300

    set terniary operator for easy and hard differenciation

--- colorpick.js
+++ colorpick.js
@@ -25,6 +25,7 @@ for (var i = 0; i < modeButtons.length; i++) {
     modeButtons[0].classList.remove("selected");
     modeButtons[1].classList.remove("selected");
     this.classList.add("selected");
+    this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
     reset();
   });
 }
